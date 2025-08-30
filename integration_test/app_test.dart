5e64ea3753fd6581546e5a89d062aeea35ec1f5b
Veerakit Chotpititaveepong <veerakit.c@7peakssoftware.com>
2025-08-30 17:04:01 +0700

    test: add invalid login test cases

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -3,6 +3,12 @@ import 'package:patrol/patrol.dart';
 import 'package:patrol_basics_tutorial/main.dart';
 import 'package:patrol_basics_tutorial/pages/integration_test_keys.dart';
 
+Future<void> nativePermissionDialogHandler($) async {
+  if (await $.native.isPermissionDialogVisible()) {
+    await $.native.grantPermissionWhenInUse();
+  }
+}
+
 void main() {
   patrolTest(
     'signs in, triggers a notification, and taps on it',
@@ -13,17 +19,48 @@ void main() {
       await $(keys.signInPage.emailTextField).enterText('test@email.com');
       await $(keys.signInPage.passwordTextField).enterText('password');
       await $(keys.signInPage.signInButton).tap();
-      if (await $.native.isPermissionDialogVisible()) {
-        await $.native.grantPermissionWhenInUse();
-      }
+      await nativePermissionDialogHandler($);
+      await $(keys.homePage.patrolLogo).waitUntilVisible();
       await $(keys.homePage.notificationIcon).tap();
       await $.native.pressHome();
       await $.native.openNotifications();
       await $.native.tapOnNotificationBySelector(
-        Selector(textContains: 'Patrol says hello!'),
-        timeout: const Duration(seconds: 5),
-      );
+          Selector(textContains: 'Patrol says hello!'));
       await $(keys.homePage.successSnackbar).waitUntilVisible();
+    },
+  );
+
+  patrolTest(
+    'invalid sign in, triggers an error message, re-login is possible',
+    framePolicy: LiveTestWidgetsFlutterBindingFramePolicy.fullyLive,
+    ($) async {
+      initApp();
+      await $.pumpWidgetAndSettle(const MainApp());
+      await $(keys.signInPage.emailTextField).enterText('test@email.com');
+      await $(keys.signInPage.passwordTextField).enterText('pass');
+      await $(keys.signInPage.signInButton).tap();
+      await $('Password must be at least 8 characters').waitUntilVisible();
+      await $(keys.signInPage.passwordTextField).enterText('password');
+      await $(keys.signInPage.signInButton).tap();
+      await nativePermissionDialogHandler($);
+      await $(keys.homePage.patrolLogo).waitUntilVisible();
+    },
+  );
+
+  patrolTest(
+    'blank sign in, triggers error messages, re-login is possible',
+    framePolicy: LiveTestWidgetsFlutterBindingFramePolicy.fullyLive,
+    ($) async {
+      initApp();
+      await $.pumpWidgetAndSettle(const MainApp());
+      await $(keys.signInPage.signInButton).tap();
+      await $('Please enter a valid email').waitUntilVisible();
+      await $('Please enter a password').waitUntilVisible();
+      await $(keys.signInPage.emailTextField).enterText('test@email.com');
+      await $(keys.signInPage.passwordTextField).enterText('password');
+      await $(keys.signInPage.signInButton).tap();
+      await nativePermissionDialogHandler($);
+      await $(keys.homePage.patrolLogo).waitUntilVisible();
     },
   );
 }
